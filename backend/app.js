e55a7137c9d0720cec32058485d67b4665f66904
Safian62 <meharsafian62@gmail.com>
2025-08-31 21:17:54 +0500

    frontend origin update

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://multivender-kzk1.vercel.app/",
     credentials: true,
   })
 );
