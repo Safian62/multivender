0ec160e95179b84bb14d7d5b6960aeda3fe93443
Safian62 <meharsafian62@gmail.com>
2025-09-01 18:02:41 +0500

    mongo issue

--- backend/db/dataBase.js
+++ backend/db/dataBase.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = () => {
-  mongoose.connect(process.env.DB_URL).then((data) => {
+  mongoose.connect("mongodb+srv://Safian:safian@cluster0.vdjtytx.mongodb.net/").then((data) => {
     console.log(`mongodb is connected with server ${data.connection.host}`);
   });
 };
