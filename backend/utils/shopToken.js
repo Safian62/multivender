434f7b00a334baaaa01519065c8b72fe4ac0783e
Safian62 <meharsafian62@gmail.com>
2025-09-01 09:56:31 +0500

    corect server url

--- backend/utils/shopToken.js
+++ backend/utils/shopToken.js
@@ -1,12 +1,12 @@
 // CREATE TOKEN AND SAVE IT IN COOKIES
 
 const sendShopToken = (user, statusCode, resp) => {
-  const token = user.getJwtToken(); // Assumes your User model has a method called getJwtToken()
+  const token = user.getJwtToken(); 
 
   // OPTIONS FOR COOKIES
   const options = {
-    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000), // 90 days
-    httpOnly: true, // Makes cookie accessible only by the web server
+    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000), 
+    httpOnly: true, 
     sameSite: "none",
     secure: true,
   };
--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://e-shop.vercel.app/api/v2";
+export const server = "https://multivender-nine.vercel.app//api/v2";
 
 export const backend_url = "https://multivender-nine.vercel.app/";
